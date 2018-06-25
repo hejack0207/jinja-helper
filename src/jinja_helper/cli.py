9309cbefca05e2f998440b173470cc2c46e52eb2
hejack0207 <hejack0207@sina.com>
2018-06-25 22:06:12 +0800

    add stdin input supports

--- src/jinja_helper/cli.py
+++ src/jinja_helper/cli.py
@@ -1,17 +1,33 @@
 from jinja2schema import infer, to_json_schema
+import sys
 import fire
 import click
 
-def sample(jinjafile):
-    with click.open_file(jinjafile) as f:
-        lines = f.readlines()
-        print(infer('\n'.join(lines)))
+def sample(jinjafile=None):
+    _process(jinjafile, True)
 
-def schema(jinjafile):
-    with click.open_file(jinjafile) as f:
-        lines = f.readlines()
+def schema(jinjafile=None):
+    _process(jinjafile, False)
+
+def _process(jinjafile, sample_or_schema=True):
+    #click.open_file(jinjafile)
+    #if sys.stdin.isatty():
+    if jinjafile is None:
+        lines = sys.stdin.readlines()
         sample = infer('\n'.join(lines))
-        print(to_json_schema(sample))
+        if sample_or_schema is True:
+            print(sample)
+        else:
+            print(to_json_schema(sample))
+    else:
+        with click.open_file(jinjafile) as f:
+            lines = f.readlines()
+            sample = infer('\n'.join(lines))
+            if sample_or_schema is True:
+                print(sample)
+            else:
+                print(to_json_schema(sample))
+
 
 if __name__ == '__main__':
     fire.Fire()
