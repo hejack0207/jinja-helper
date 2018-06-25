46f2bfd7487ed289ae79941f1cee86f9a2d845f6
hejack0207 <hejack0207@sina.com>
2018-06-25 14:33:17 +0800

    add schema sub command

--- src/jinja_helper/cli.py
+++ src/jinja_helper/cli.py
@@ -1,8 +1,16 @@
 from jinja2schema import infer, to_json_schema
 import fire
 
-def sample():
-    pass
+def sample(jinjafile):
+    with open(jinjafile) as f:
+        lines = f.readlines()
+        print(infer('\n'.join(lines)))
+
+def schema(jinjafile):
+    with open(jinjafile) as f:
+        lines = f.readlines()
+        sample = infer('\n'.join(lines))
+        print(to_json_schema(sample))
 
 if __name__ == '__main__':
     fire.Fire()
