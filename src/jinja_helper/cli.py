3b1e8b72ec09a2fc8c530109b7e10a5a0c4f0e51
hejack0207 <hejack0207@sina.com>
2018-06-25 21:50:35 +0800

    add stdin input supports

--- src/jinja_helper/cli.py
+++ src/jinja_helper/cli.py
@@ -1,13 +1,14 @@
 from jinja2schema import infer, to_json_schema
 import fire
+import click
 
 def sample(jinjafile):
-    with open(jinjafile) as f:
+    with click.open_file(jinjafile) as f:
         lines = f.readlines()
         print(infer('\n'.join(lines)))
 
 def schema(jinjafile):
-    with open(jinjafile) as f:
+    with click.open_file(jinjafile) as f:
         lines = f.readlines()
         sample = infer('\n'.join(lines))
         print(to_json_schema(sample))
